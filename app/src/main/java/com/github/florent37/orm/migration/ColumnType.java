ffe2bf47024221192199e2dece744a4da13ad2bc
florent champigny <fchampigny@xebia.fr>
2016-01-28 16:08:40 +0100

    added some migration code

--- app/src/main/java/com/github/florent37/orm/migration/ColumnType.java
+++ app/src/main/java/com/github/florent37/orm/migration/ColumnType.java
@@ -3,23 +3,51 @@ package com.github.florent37.orm.migration;
 /**
  * Created by florentchampigny on 28/01/2016.
  */
-public enum  ColumnType {
-    Int,
-    Float,
-    Boolean,
-    String,
+public enum ColumnType {
 
-    ListOfInts,
-    ListOfFloats,
-    ListOfBooleans,
-    ListOfStrings,
+    Int(FieldType.TYPE_PRIMITIVE,"number"),
+    Float(FieldType.TYPE_PRIMITIVE, "real"),
+    Boolean(FieldType.TYPE_PRIMITIVE, "number"),
+    String(FieldType.TYPE_PRIMITIVE, "text"),
 
-    ArrayOfInts,
-    ArrayOfFloats,
-    ArrayOfBooleans,
-    ArrayOfStrings;
+    ListOfInts(FieldType.TYPE_COLLECTION, null),
+    ListOfFloats(FieldType.TYPE_COLLECTION, null),
+    ListOfBooleans(FieldType.TYPE_COLLECTION, null),
+    ListOfStrings(FieldType.TYPE_COLLECTION, null),
+
+    ArrayOfInts(FieldType.TYPE_ARRAY, null),
+    ArrayOfFloats(FieldType.TYPE_ARRAY, null),
+    ArrayOfBooleans(FieldType.TYPE_ARRAY, null),
+    ArrayOfStrings(FieldType.TYPE_ARRAY, null),
+
+    Custom(FieldType.TYPE_CUSTOM, null)
+    ;
+
+    private int type;
+    private String sqlName;
+
+    ColumnType(int type, String sqlName) {
+        this.type = type;
+        this.sqlName = sqlName;
+    }
 
     public static ColumnType ofTable(String objectName) {
         return null; //TODO
+    }
+
+    public boolean isPrimitive(){
+        return type == FieldType.TYPE_PRIMITIVE;
+    }
+
+    public boolean isArray(){
+        return type == FieldType.TYPE_ARRAY;
+    }
+
+    public boolean isCollection(){
+        return type == FieldType.TYPE_COLLECTION;
+    }
+
+    public java.lang.String getSqlName() {
+        return sqlName;
     }
 }
--- app/src/main/java/com/github/florent37/orm/migration/FreezerMigrator.java
+++ app/src/main/java/com/github/florent37/orm/migration/FreezerMigrator.java
@@ -1,11 +1,18 @@
 package com.github.florent37.orm.migration;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * Created by florentchampigny on 28/01/2016.
  */
 public class FreezerMigrator {
     public static TableCreator createObject(String objectName) {
         return new TableCreator(objectName);
+    }
+
+    protected static String getTableName(String objectName) {
+        return objectName.toUpperCase();
     }
 
     public TableTransformer update(String objectName) {
@@ -20,18 +27,33 @@ public class FreezerMigrator {
     public static class TableTransformer {
         String objectName;
 
+        List<String> operation = new ArrayList<String>();
+
         public TableTransformer(String objectName) {
-            //TODO
             this.objectName = objectName;
         }
 
         public ColumnTransformer transform(String column) {
-            //TODO
-            return new ColumnTransformer(this, column);
+            return new ColumnTransformer(this, objectName, column);
         }
 
         public TableTransformer name(String newName) {
-            //TODO
+            operation.add(String.format("ALTER TABLE %s RENAME TO %s", getTableName(objectName), getTableName(newName)));
+            return this;
+        }
+
+        public TableTransformer removeField(String column) {
+            operation.add(String.format("ALTER TABLE %s REMOVE COLUMN %s", getTableName(objectName), column));
+            return this;
+        }
+
+        public TableTransformer addField(String column, ColumnType type) {
+            //primitive
+            if (type.isPrimitive()) {
+                operation.add(String.format("ALTER TABLE %s ADD COLUMN %s %s", getTableName(objectName), column, type.getSqlName()));
+            }
+
+            //else TODO
             return this;
         }
 
@@ -39,29 +61,26 @@ public class FreezerMigrator {
             //TODO
         }
 
-        public TableTransformer removeField(String column) {
-            //TODO
-            return this;
-        }
-
-        public TableTransformer addField(String column, ColumnType type) {
-            //TODO
-            return this;
-        }
-
     }
 
     public static class ColumnTransformer {
         TableTransformer tableTransformer;
         String columnName;
+        String objectName;
 
-        public ColumnTransformer(TableTransformer tableTransformer, String columnName) {
+        List<String> operation = new ArrayList<String>();
+
+        public ColumnTransformer(TableTransformer tableTransformer, String objectName, String columnName) {
             this.tableTransformer = tableTransformer;
+            this.objectName = objectName;
             this.columnName = columnName;
         }
 
         public TableTransformer name(String newName) {
-            //TODO
+            //primitive
+            operation.add(String.format("ALTER TABLE %s RENAME COLUMN %s TO %s", getTableName(objectName), columnName, newName));
+
+            //else TODO
             return tableTransformer;
         }
 
