e4c7011ee41934fe82e0cfe2768d4afee0171d19
florent champigny <fchampigny@xebia.fr>
2016-01-29 17:07:48 +0100

    added MainActivity.queries()

--- app/src/main/java/com/github/florent37/orm/DatabaseMigration.java
+++ app/src/main/java/com/github/florent37/orm/DatabaseMigration.java
@@ -9,38 +9,38 @@ import fr.xebia.android.freezer.migration.Migrator;
  */
 public class DatabaseMigration {
 
-    @Migration(2)
-    public static void migrateTo2(Migrator migrator) {
-        migrator.update("User")
-                .removeField("age")
-                .renameTo("Man");
-    }
+    //@Migration(2)
+    //public static void migrateTo2(Migrator migrator) {
+    //    migrator.update("User")
+    //            .removeField("age")
+    //            .renameTo("Man");
+    //}
 
-    @Migration(3)
-    public static void migrateTo3(Migrator migrator) {
-        migrator.update("Man")
-                .addField("birth", ColumnType.Primitive.Int);
-    }
+    //@Migration(3)
+    //public static void migrateTo3(Migrator migrator) {
+    //    migrator.update("Man")
+    //            .addField("birth", ColumnType.Primitive.Int);
+    //}
 
-    @Migration(4)
-    public static void migrateTo4(Migrator migrator) {
-        migrator.update("Man")
-                .transform("name").renameTo("myName")
-                .addField("lastName", ColumnType.Primitive.String);
-    }
+    //@Migration(4)
+    //public static void migrateTo4(Migrator migrator) {
+    //    migrator.update("Man")
+    //            .transform("name").renameTo("myName")
+    //            .addField("lastName", ColumnType.Primitive.String);
+    //}
 
-    @Migration(5)
-    public static void migrateTo5(Migrator migrator) {
-        migrator.addTable(migrator.createModel("Woman")
-                        .field("name", ColumnType.Primitive.String)
-                        .build());
-    }
+    //@Migration(5)
+    //public static void migrateTo5(Migrator migrator) {
+    //    migrator.addTable(migrator.createModel("Woman")
+    //                    .field("name", ColumnType.Primitive.String)
+    //                    .build());
+    //}
 
-    @Migration(6)
-    public static void migrateTo6(Migrator migrator) {
-        migrator.update("Woman")
-                .addField("mans", ColumnType.collectionOfModel("Man"))
-                .addField("age", ColumnType.Primitive.Int);
-    }
+    //@Migration(6)
+    //public static void migrateTo6(Migrator migrator) {
+    //    migrator.update("Woman")
+    //            .addField("mans", ColumnType.collectionOfModel("Man"))
+    //            .addField("age", ColumnType.Primitive.Int);
+    //}
 
 }
--- app/src/main/java/com/github/florent37/orm/MainActivity.java
+++ app/src/main/java/com/github/florent37/orm/MainActivity.java
@@ -4,10 +4,19 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 
+import com.github.florent37.orm.model.Cat;
 import com.github.florent37.orm.model.CatEntityManager;
+import com.github.florent37.orm.model.Dog;
+import com.github.florent37.orm.model.User;
+import com.github.florent37.orm.model.UserEntityManager;
+
+import fr.xebia.android.freezer.QueryLogger;
+
+import java.util.Arrays;
 
 public class MainActivity extends AppCompatActivity {
 
+    UserEntityManager userORM;
     CatEntityManager catORM;
 
     @Override
@@ -18,10 +27,31 @@ public class MainActivity extends AppCompatActivity {
         queries();
     }
 
-    protected void queries() {
+    protected void queries(){
+        userORM = new UserEntityManager();
         catORM = new CatEntityManager();
 
-        Log.d("SQL", catORM.select().asList().toString());
+        userORM.logQueries(new QueryLogger() {
+            @Override public void onQuery(String query, String[] datas) {
+                Log.d("QUERY", query);
+            }
+        });
+
+        userORM.deleteAll();
+        userORM.add(Arrays.asList(
+                        new User(21, "florent", new Cat("Java"), Arrays.asList(new Dog("Loulou")), true),
+                        new User(30, "kevin", new Cat("Futé"), Arrays.asList(new Dog("Darty")), true),
+                        new User(10, "alex", new Cat("Yellow"), Arrays.asList(new Dog("Darty"), new Dog("Sasha")), false)
+                )
+        );
+
+        Log.d("DAO", userORM.select()
+                .hacker().isTrue()
+                .or()
+                .age().equalsTo(4)
+
+                .asList()
+                .toString());
 
     }
 }
